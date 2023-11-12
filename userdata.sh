b0319fddf049b7fa39df5a05b32da43fbc364cb1
hasan-ilker <hasanilkerunver@gmail.com>
2023-11-12 04:04:20 +0300

    modify user data

--- userdata.sh
+++ userdata.sh
@@ -6,12 +6,12 @@ apt install python3-pip -y
 pip3 install boto3
 apt  install awscli -y
 cd /home/ubuntu/
-TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /fsmehmet/capstone/token --with-decryption --query 'Parameter.Value' --output text)
-git clone https://$TOKEN@github.com/FS-MEHMET/AWS-Capstone-Django.git
-cd /home/ubuntu/AWS-Capstone-Django
+TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /hasan/capstone/token --with-decryption --query 'Parameter.Value' --output text)
+git clone https://$TOKEN@github.com/Hasan-ilker/hsn-capstone-project.git
+cd /home/ubuntu/hsn-capstone-project
 apt-get install python3.10-dev default-libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/AWS-Capstone-Django/src
+cd /home/ubuntu/hsn-capstone-project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
